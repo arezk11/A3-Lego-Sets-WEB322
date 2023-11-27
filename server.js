759ddfe1b2d3d8f98bf12b4641176cc4d84d29b5
ALi Rezk <133886728+arezk11@users.noreply.github.com>
2023-11-26 19:02:01 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /********************************************************************************
-*  WEB322 – Assignment 04
+*  WEB322 – Assignment 05
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
@@ -12,8 +12,6 @@
 *
 ********************************************************************************/
 
-
-
 const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
@@ -21,6 +19,7 @@ const HTTP_PORT = process.env.PORT || 8080;
 const legoData = require("./modules/legoSets");
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -62,11 +61,75 @@ app.get("/lego/sets/:set_num", (req, res) => {
     });
 });
 
+app.get("/lego/addSet", (req, res) => {
+  legoData
+    .getAllThemes()
+    .then((themeData) => res.render("addSet", { themes: themeData }))
+    .catch((err) =>
+      res.status(404).render("404", {
+        message: `${err.message}`,
+      })
+    );
+});
+
+app.post("/lego/addSet", (req, res) => {
+  legoData.addSet(req.body) 
+    .then(() => res.redirect("/lego/sets"))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      });
+    });
+});
+
+app.get('/lego/editSet/:num', async (req, res) => {
+  try {
+    const [set, themes] = await Promise.all([
+      legoData.getSetByNum(req.params.num),
+      legoData.getAllThemes(),
+    ]);
+
+    res.render('editSet', { set, themes });
+  } catch (error) {
+    res.status(404).render('404', { message: error.message });
+  }
+});
+
+// POST route to update the set
+app.post('/lego/editSet', async (req, res) => {
+  try {
+    const setData = {
+      name: req.body.name,
+      year: req.body.year,
+      num_parts: req.body.num_parts,
+      img_url: req.body.img_url,
+      theme_id: req.body.theme_id,
+      set_num: req.body.set_num,
+    };
+
+    await legoData.editSet(req.body.set_num, setData);
+
+    res.redirect('/lego/sets');
+  } catch (error) {
+    res.status(500).render('500', { message: `Error editing set: ${error.message}` });
+  }
+});
+
+app.get("/lego/deleteSet/:num", async (req, res) => {
+  legoData
+    .deleteSet(req.params.num)
+    .then(() => res.redirect("/lego/sets"))
+    .catch((err) =>
+      res.render("500", {
+        message: `I'm sorry, but we have encountered the following error: ${err}`,
+      })
+    );
+});
 
 app.use((req, res) => {
   res.status(404).render("404", { message: "I'm sorry, we're unable to find what you're looking for" });
 });
-
 
 legoData.Initialize()
   .then(() => {
